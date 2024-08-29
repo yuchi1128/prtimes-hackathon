7605e28969d4649d437a0de35555d43c9a3e9479
Snoda2023 <tk220137syotaro.noda@gmail.com>
2024-08-29 16:41:32 +0900

    add login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 
-import React, { useState, FormEvent } from 'react'
+import React, { useState } from 'react'
 import { Input } from '@/components/ui/input'
 import { UserIcon, LockClosedIcon } from '@heroicons/react/24/solid'
 import { Button } from '@/components/ui/button'
+import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
 
+  const router = useRouter()
   const [loginData, setLoginData] = useState({
     username: "",
     password: ""
   });
   const [nowLoading, setNowLoading] = useState(false)
-
+  const [errorMsg, setErrorMsg] = useState("")
+ 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const {name, value} = e.target;
     setLoginData((prevData) => ({
@@ -22,14 +25,34 @@ export default function LoginPage() {
   };
 
   async function handleLogin(){
-    
+    setNowLoading(true)
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginData),
+      });
+      const jsonData = await response.json()
+      if (response.status === 200) {
+        localStorage.setItem("userId", jsonData["data"]["id"])
+        router.push("/")
+      }
+      setErrorMsg(jsonData.message)
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setNowLoading(false)
+    }
   }
   
   return (
     <div className="min-h-screen w-full bg-gray-300">
       <div className="flex justify-center">
-        <div className="bg-white rouded-lg mt-16  rounded-lg flex flex-col items-center">
-          <div className="px-9 pt-7">
+        <div className="bg-white rounded-xl mt-16 flex flex-col items-center">
+          <div className="px-9 pt-7 rounded">
             <div className="text-lg font-bold w-full text-center">ユーザーログイン</div>
             <div className="flex items-center relative mt-5">
               <UserIcon className="w-5 h-5 text-gray-400 absolute left-3" />  
